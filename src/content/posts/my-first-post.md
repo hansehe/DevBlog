e184abda0bd97003a5ea05a166d298985cf7e6f4
Hans Erik Heggem <hans.erik.heggem@gmail.com>
2018-12-15 19:30:19 +0100

    new theme

--- src/content/posts/my-first-post.md
+++
@@ -1,6 +0,0 @@
----
-title: "My First Post"
-date: 2018-10-21T19:52:37+02:00
-draft: true
----
-
--- src/init_letsencrypt.sh
+++ src/init_letsencrypt.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-domains=( "example.org" "example.com" )
+domains=( "codingplot.com" "www.codingplot.com" )
 rsa_key_size=4096
 data_path="./data/certbot"
-email="" #Adding a valid address is strongly recommended 
+email="hans.erik.heggem@gmail.com" #Adding a valid address is strongly recommended 
 staging=0 #Set to 1 if you're just testing your setup to avoid hitting request limits
 
 echo "### Preparing directories in $data_path ..."
